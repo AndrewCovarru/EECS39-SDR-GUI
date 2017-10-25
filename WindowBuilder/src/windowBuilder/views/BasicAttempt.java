b110c9611b049e2fe529ad1775d5ae3fddc33e5b
AndrewCovarru <AndrewCovarru@gmail.com>
2017-10-25 01:53:33 -0400

    Added sliders, keypad, and more!

--- WindowBuilder/src/windowBuilder/views/BasicAttempt.java
+++ WindowBuilder/src/windowBuilder/views/BasicAttempt.java
@@ -1,6 +1,7 @@
 package windowBuilder.views;
 
 import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -11,6 +12,8 @@ import javax.swing.JCheckBox;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener; 
 import javax.swing.JSeparator;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -27,6 +30,14 @@ import javax.swing.ButtonGroup;
 import javax.swing.DropMode;
 import javax.swing.JFormattedTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JSlider;
+import javax.swing.border.EtchedBorder;
+import java.awt.SystemColor;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 
 public class BasicAttempt extends JFrame {
 
@@ -60,140 +71,143 @@ public class BasicAttempt extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JSeparator separator = new JSeparator();
+		JFormattedTextField freqDisplay = new JFormattedTextField();
+		freqDisplay.setHorizontalAlignment(SwingConstants.CENTER);
+		freqDisplay.setForeground(new Color(255, 250, 250));
+		freqDisplay.setBackground(new Color(0, 0, 0));
+		freqDisplay.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 42));
+		freqDisplay.setEditable(false);
+		freqDisplay.setText("Frequency");
 		
-		JRadioButton btnNewButton = new JRadioButton("Mode");
-		buttonGroup.add(btnNewButton);
-		btnNewButton.setForeground(new Color(255, 250, 250));
-		btnNewButton.setBackground(new Color(128, 128, 128));
-		btnNewButton.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
+		JFormattedTextField gainField = new JFormattedTextField();
+		gainField.setEditable(false);
 		
-		JRadioButton btnFrequenc = new JRadioButton("Frequency");
-		buttonGroup.add(btnFrequenc);
-		btnFrequenc.setForeground(new Color(255, 250, 250));
-		btnFrequenc.setBackground(new Color(128, 128, 128));
-		btnFrequenc.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
+		JFormattedTextField volField = new JFormattedTextField();
+		volField.setEditable(false);
 		
-		JRadioButton btnGainvolume = new JRadioButton("Volume");
-		buttonGroup.add(btnGainvolume);
-		btnGainvolume.setForeground(new Color(255, 250, 250));
-		btnGainvolume.setBackground(new Color(128, 128, 128));
-		btnGainvolume.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
+		JFormattedTextField squField = new JFormattedTextField();
+		squField.setEditable(false);
 		
-		JRadioButton btnUnits = new JRadioButton("Units");
-		buttonGroup.add(btnUnits);
-		btnUnits.setForeground(new Color(255, 250, 250));
-		btnUnits.setBackground(new Color(128, 128, 128));
-		btnUnits.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
+		JSlider gainSlider = new JSlider();
+		gainSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				gainField.setText("" + gainSlider.getValue());
+			}
+		});
+		gainSlider.setBackground(new Color(105, 105, 105));
+		gainSlider.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
 		
-		JButton btnIncrease = new JButton("Increase");
-		btnIncrease.addActionListener(new ActionListener() {
+		
+		JSlider volSlider = new JSlider();
+		volSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				volField.setText("" + volSlider.getValue());
+			}
+		});
+		volSlider.setBackground(new Color(105, 105, 105));
+		volSlider.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
+		
+		JSlider squSlider = new JSlider();
+		squSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				squField.setText("" + squSlider.getValue());
+			}
+		});
+		squSlider.setBackground(new Color(105, 105, 105));
+		squSlider.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
+		
+		JLabel volLabel = new JLabel("Volume: ");
+		volLabel.setForeground(new Color(255, 250, 250));
+		volLabel.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
+		
+		JLabel gainLabel = new JLabel("Gain:");
+		gainLabel.setForeground(new Color(255, 250, 250));
+		
+		JLabel squLabel = new JLabel("Squelch:");
+		squLabel.setForeground(new Color(255, 250, 250));
+		
+		JButton btnFrequency = new JButton("Frequency");
+		btnFrequency.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				KeyPad keypad = new KeyPad();
+				keypad.setVisible(true);
+				
+				 keypad.addWindowListener(new WindowListener() {
+			            public void windowClosed(WindowEvent arg0) {
+			                freqDisplay.setText(keypad.returnFreq());
+			            }
+			            public void windowActivated(WindowEvent arg0) {
+			            }
+			            public void windowClosing(WindowEvent arg0) {
+			            }
+			            public void windowDeactivated(WindowEvent arg0) {
+			            }
+			            public void windowDeiconified(WindowEvent arg0) {
+			            }
+			            public void windowIconified(WindowEvent arg0) {
+			            }
+			            public void windowOpened(WindowEvent arg0) {
+			            }
+			        });
+				
 			}
 		});
-		btnIncrease.setBackground(new Color(128, 128, 128));
-		btnIncrease.setForeground(new Color(255, 250, 250));
-		btnIncrease.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
 		
-		JButton btnDefault = new JButton("Default");
-		btnDefault.setForeground(new Color(255, 250, 250));
-		btnDefault.setBackground(new Color(128, 128, 128));
-		btnDefault.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnDefault.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
 		
-		JButton btnDecrease = new JButton("Decrease");
-		btnDecrease.setForeground(new Color(255, 250, 250));
-		btnDecrease.setBackground(new Color(128, 128, 128));
-		btnDecrease.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 11));
-		
-		JLabel lblNewLabel = new JLabel("Frequency Range (AM, FM, USB, LSB)");
-		lblNewLabel.setForeground(new Color(255, 250, 250));
-		lblNewLabel.setFont(new Font("Segoe UI Symbol", Font.BOLD, 11));
-		
-		JLabel lblNetworkStatus = new JLabel("Network Status");
-		lblNetworkStatus.setForeground(new Color(255, 250, 250));
-		lblNetworkStatus.setFont(new Font("Segoe UI Symbol", Font.BOLD, 11));
-		
-		JRadioButton btnGain = new JRadioButton("Gain");
-		buttonGroup.add(btnGain);
-		btnGain.setForeground(new Color(255, 250, 250));
-		btnGain.setBackground(new Color(128, 128, 128));
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBackground(new Color(128, 128, 128));
-		comboBox.setForeground(new Color(255, 250, 250));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"WBFM", "AM", "USB", "LSB"}));
-		comboBox.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 11));
-		comboBox.setMaximumRowCount(4);
-		
-		JFormattedTextField frmtdtxtfldHelloWorld = new JFormattedTextField();
-		frmtdtxtfldHelloWorld.setHorizontalAlignment(SwingConstants.CENTER);
-		frmtdtxtfldHelloWorld.setForeground(new Color(255, 250, 250));
-		frmtdtxtfldHelloWorld.setBackground(new Color(0, 0, 0));
-		frmtdtxtfldHelloWorld.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 67));
-		frmtdtxtfldHelloWorld.setText("92100 Hz\r\n");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(separator, GroupLayout.PREFERRED_SIZE, 424, GroupLayout.PREFERRED_SIZE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(frmtdtxtfldHelloWorld, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 314, Short.MAX_VALUE)
-								.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-										.addComponent(btnUnits, GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
-										.addComponent(btnFrequenc, GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE))
-									.addGap(18)
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-										.addComponent(btnGain, GroupLayout.DEFAULT_SIZE, 77, Short.MAX_VALUE))
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(btnGainvolume, GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE)
-										.addComponent(comboBox, 0, 117, Short.MAX_VALUE)))
-								.addComponent(lblNewLabel))
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(volSlider, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(squSlider, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(volLabel)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnDefault, GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-								.addComponent(btnDecrease, GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-								.addComponent(btnIncrease, GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-								.addComponent(lblNetworkStatus, GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE))))
-					.addGap(225))
+							.addComponent(volField, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+						.addComponent(freqDisplay, Alignment.LEADING)
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(gainLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(gainField, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(squLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(squField, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+						.addComponent(gainSlider, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnFrequency))
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(lblNetworkStatus, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnIncrease)
-							.addGap(18)
-							.addComponent(btnDefault)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(btnDecrease))
-						.addComponent(frmtdtxtfldHelloWorld, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnGainvolume)
-						.addComponent(btnNewButton)
-						.addComponent(btnFrequenc))
-					.addGap(9)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnUnits)
-						.addComponent(btnGain, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(300)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(6))
+							.addComponent(btnFrequency)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(freqDisplay, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(volLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(volField, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(volSlider, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addGap(5)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE, false)
+								.addComponent(gainLabel)
+								.addComponent(gainField, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(gainSlider, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(squLabel)
+								.addComponent(squField, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(squSlider, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(6))))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
